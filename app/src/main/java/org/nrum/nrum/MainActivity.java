023b415bf16a159dc93a8acc90c94f44d5bd25a4
mahendra.rajdhami <mahendra@miracleinterface.com>
2016-12-24 16:03:36 +0545

    [ADDED]: --> slider images

--- app/src/main/java/org/nrum/nrum/MainActivity.java
+++ app/src/main/java/org/nrum/nrum/MainActivity.java
@@ -57,14 +57,10 @@ public class MainActivity extends AppCompatActivity
         HashMap<String,String> url_maps = new HashMap<String, String>();
         url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
         url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
-        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
-        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
         HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("Hannibal",R.drawable.hannibal);
-        file_maps.put("Big Bang Theory",R.drawable.bigbang);
-        file_maps.put("House of Cards",R.drawable.house);
-        file_maps.put("Game of Thrones", R.drawable.game_of_thrones);
+        file_maps.put("Hannibal",R.drawable.slider1);
+        file_maps.put("Big Bang Theory",R.drawable.slider2);
 
         for(String name : file_maps.keySet()){
             TextSliderView textSliderView = new TextSliderView(this);
