f2c6822b6b48f1841d7709325fde1aa42411c56e
mahendra.rajdhami <mahendra@miracleinterface.com>
2016-12-25 08:14:17 +0545

    [ADDED]: --> Refresh Button In Web View

--- app/src/main/java/org/nrum/nrum/NrumWebViewActivity.java
+++ app/src/main/java/org/nrum/nrum/NrumWebViewActivity.java
@@ -2,6 +2,7 @@ package org.nrum.nrum;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -59,6 +60,16 @@ public class NrumWebViewActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(NrumWebViewActivity.this, "No Internet Connection", Toast.LENGTH_LONG);
             toast.show();
         }
+        // action for refreshWeb
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.refreshWeb);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // refresh Activity
+                finish();
+                startActivity(getIntent());
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
