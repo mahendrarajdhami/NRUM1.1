f793da5f2cf8a73ac10524179d19be8bc5d7d065
mahendra.rajdhami <mahendra@miracleinterface.com>
2017-01-07 01:35:02 +0545

    [FIXED]: --> ToolBar color

--- app/src/main/java/org/nrum/nrum/CalendarActivity.java
+++ app/src/main/java/org/nrum/nrum/CalendarActivity.java
@@ -2,9 +2,9 @@ package org.nrum.nrum;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.CalendarView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
     CalendarView calendarView;
@@ -15,14 +15,10 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
-                dateDisplay.setText("Date: " + i2 + " / " + i1 + " / " + i);
-
-                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
-            }
-        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 }
